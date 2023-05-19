c7cfcf8ab31912d68b729f13af2d1a674f4140c8
larry-noriega <larry.noriega@hotmail.com>
2023-05-19 09:36:29 -0500

    feat: Models Added.

---
+++ EFCoreSQLite/Entities/Blog.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreSQLite.Entities
+{
+	public class Blog
+	{
+		public int BlogId { get; set; }
+		public string? Url { get; set; }
+
+		public List<Post> Posts { get; } = new();
+	}
+}
